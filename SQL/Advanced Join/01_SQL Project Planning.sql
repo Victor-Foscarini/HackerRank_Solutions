5a7c0ae22cdea087b4eaaefccfc4a5f496686fb2
Victor Foscarini <68918292+Victor-Foscarini@users.noreply.github.com>
2021-01-21 15:45:51 -0300

    Update 01_SQL Project Planning.sql

--- SQL/Advanced Join/01_SQL Project Planning.sql
+++ SQL/Advanced Join/01_SQL Project Planning.sql
@@ -1,6 +1,19 @@
+-- solution 1: alterins sql mode of using group by
+
 set sql_mode = '';
 select Start_Date, End_Date from
 (select Start_Date from Projects where Start_Date not in (select End_Date from Projects)) s,
 (select End_Date from Projects where End_Date not in (select Start_Date from Projects)) e
 where Start_Date < End_Date
 group by Start_Date order by DATEDIFF(End_Date,Start_Date),Start_Date;
+
+                                                      
+-- solution 2: using cross join and min  
+                                                      
+set sql_mode = '';
+select Start_Date, End_Date from
+(select Start_Date from Projects where Start_Date not in (select End_Date from Projects)) s,
+(select End_Date from Projects where End_Date not in (select Start_Date from Projects)) e
+where Start_Date < End_Date
+group by Start_Date order by DATEDIFF(End_Date,Start_Date),Start_Date;
+                                                      
