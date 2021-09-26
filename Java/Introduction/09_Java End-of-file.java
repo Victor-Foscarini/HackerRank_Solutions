fb19bba819b2939103c32290090da4a1a0a8cd70
Victor Foscarini <68918292+Victor-Foscarini@users.noreply.github.com>
2021-09-26 09:35:14 -0300

    Create 09_Java End-of-file.java

---
+++ Java/Introduction/09_Java End-of-file.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n=0;
+        while (scan.hasNext()) {
+            n+=1;
+            System.out.println(n+" "+scan.nextLine());
+        }
+    }
+}
